c3b917c70e0cee83086c7947c8208cacd4120290
BUPTyj <1348227434@bupt.edu.cn>
2025-06-27 22:00:40 +0800

    add cnpy

---
+++ cnpy/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/main.cpp.o"
+  "CMakeFiles/test.dir/main.cpp.o.d"
+  "libtest.dll.a"
+  "test.exe"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
